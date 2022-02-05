2e33311f4a04cc0eb989d076cb13e69a644728c7
humam-kourani <humam.kourani@gmail.com>
2022-02-05 14:46:10 +0000

    dialogs updated
    git-svn-id: https://svn.win.tue.nl/repos/prom/Packages/ExtendedHybridMiner/Trunk@45844 a68f48d4-8d00-424f-b812-3f22a534a02a

--- src/org/processmining/extendedhybridminer/dialogs/CGDialog.java
+++ src/org/processmining/extendedhybridminer/dialogs/CGDialog.java
@@ -20,47 +20,27 @@ public class CGDialog extends JPanel {
 
 	private static final long serialVersionUID = -3587595452197538653L;
 	@SuppressWarnings("unused")
-	//private FuzzyCGConfiguration configuration;
 	private HybridCGMinerSettings settings;
 
 	public CGDialog(UIPluginContext context, XLog log, 
 			final HybridCGMinerSettings settings) {
+		
 		super(new GridLayout(6,2,80,30));
 		this.settings = settings;
 		
-		//final JPanel label1 = new JPanel(new GridLayout(3,1,1,-10));
-		//EmptyBorder panelBorder = new EmptyBorder(10, 5, 10, 10);
-	    //label1.setBorder(panelBorder);
-	    
 		final JLabel positiveObservationsLabel = new JLabel();
 		positiveObservationsLabel.setText("<html><b><font size= '+1'>Minimal activity frequency:</font></b><br>an activitiy will be included if it occurs in at least x% of cases; <br>set to 0 to include all activities.</html>");
 		this.add(positiveObservationsLabel);
 		
-/*		final JLabel positiveObservationsLabel2 = new JLabel();
-		positiveObservationsLabel2.setText("(an activity should occur at least in x cases: ");
-		label1.add(positiveObservationsLabel2);
-		
-		final JLabel positiveObservationsLabel3 = new JLabel();
-		positiveObservationsLabel3.setText("set to 0 to include all activities)");
-		label1.add(positiveObservationsLabel3);		
-
-		//label1.setBackground(new Color(150f,150f,150f));
-		//label1.setBackground(Color.lightGray);
-		this.add(label1);
-*/		
 		final JSlider positiveObservationsSlider = new JSlider();
 		positiveObservationsSlider.setMinimum(0);
 		positiveObservationsSlider.setMaximum(100);
-		
 		int positiveObservationsValue = (int) (settings.getFilterAcivityThreshold() * 100);
 		positiveObservationsSlider.setValue(positiveObservationsValue);
 		positiveObservationsSlider.setMajorTickSpacing(50);
 		positiveObservationsSlider.setMinorTickSpacing(10);
 		positiveObservationsSlider.setPaintTicks(true);
 		Hashtable<Integer, JLabel> activitylabelTable = new Hashtable<Integer, JLabel>();
-		/*activitylabelTable.put(0, new JLabel("0%"));
-		activitylabelTable.put(50, new JLabel("50%"));
-		activitylabelTable.put(100, new JLabel("100%"));*/
 		activitylabelTable.put(positiveObservationsValue, new JLabel(positiveObservationsValue +"%"));
 		positiveObservationsSlider.setLabelTable(activitylabelTable);
 		positiveObservationsSlider.setPaintLabels(true);
@@ -81,8 +61,6 @@ public class CGDialog extends JPanel {
 		space = Box.createHorizontalStrut(10);
 		this.add(space);
 		
-		//final JPanel label2 = new JPanel(new GridLayout(3,1,1,-10));
-
 		final JLabel traceFilterLabel = new JLabel();
 		traceFilterLabel.setText("<html><b><font size= '+1'>Minimal trace variant frequency:</font></b><br>a trace variant will be included if it covers at least x% of cases; <br>set to 0 to include all trace variants.</html>");
 		this.add(traceFilterLabel);
@@ -90,7 +68,6 @@ public class CGDialog extends JPanel {
 		final JSlider traceFilterSlider = new JSlider();
 		traceFilterSlider.setMinimum(0);
 		traceFilterSlider.setMaximum(100);
-		
 		int traceFilterValue = (int) (settings.getTraceVariantsThreshold() *100);
 		traceFilterSlider.setValue(traceFilterValue);
 		traceFilterSlider.addChangeListener(new ChangeListener() {
@@ -106,9 +83,6 @@ public class CGDialog extends JPanel {
 		traceFilterSlider.setMinorTickSpacing(10);
 		traceFilterSlider.setPaintTicks(true);
 		Hashtable<Integer, JLabel> tracelabelTable = new Hashtable<Integer, JLabel>();
-		//tracelabelTable.put(0, new JLabel("0%"));
-		//tracelabelTable.put(50, new JLabel("50%"));
-		//tracelabelTable.put(100, new JLabel("100%"));
 		tracelabelTable.put(traceFilterValue, new JLabel(traceFilterValue +"%"));
 		traceFilterSlider.setLabelTable(tracelabelTable);
 		traceFilterSlider.setPaintLabels(true);
@@ -120,29 +94,13 @@ public class CGDialog extends JPanel {
 		space = Box.createHorizontalStrut(10);
 		this.add(space);
 		
-		
 		final JLabel sureThresholdLabel = new JLabel();
 		sureThresholdLabel.setText("<html><b><font size= '+1'>Strong causality threshold:</font></b><br>lower bound for a strong causality relation between two activities.</html>");
 		this.add(sureThresholdLabel);
 		
-/*		final JLabel sureThresholdLabel2 = new JLabel();
-		sureThresholdLabel2.setText("(lower bound for a strong causality relation ");
-		label2.add(sureThresholdLabel2);
-
-		final JLabel sureThresholdLabel3 = new JLabel();
-		sureThresholdLabel3.setText(" between two activities)");
-		label2.add(sureThresholdLabel3);
-
-		this.add(label2);*/
-
-		
-		//space = Box.createHorizontalStrut(10);
-		//this.add(space);
-		
 		final JSlider sureThresholdSlider = new JSlider();
 		sureThresholdSlider.setMinimum(0);
 		sureThresholdSlider.setMaximum(100);
-		
 		int sureValue = (int) (settings.getSureThreshold()*100);
 		sureThresholdSlider.setValue(sureValue);
 		sureThresholdSlider.addChangeListener(new ChangeListener() {
@@ -158,15 +116,10 @@ public class CGDialog extends JPanel {
 		sureThresholdSlider.setMinorTickSpacing(10);
 		sureThresholdSlider.setPaintTicks(true);
 		Hashtable<Integer, JLabel> surelabelTable = new Hashtable<Integer, JLabel>();
-		//surelabelTable.put(0, new JLabel("0%"));
-		//surelabelTable.put(50, new JLabel("50%"));
-		//surelabelTable.put(100, new JLabel("100%"));
 		surelabelTable.put(sureValue, new JLabel(sureValue +"%"));
-		
 		sureThresholdSlider.setLabelTable(surelabelTable);
 		sureThresholdSlider.setPaintLabels(true);
 		this.add(sureThresholdSlider);
-		
 		
 		space = Box.createVerticalStrut(20);
 		this.add(space);
@@ -174,40 +127,19 @@ public class CGDialog extends JPanel {
 		space = Box.createHorizontalStrut(20);
 		this.add(space);
 
-		
-		//final JPanel label3 = new JPanel(new GridLayout(3,1,1,-10));
-
 		final JLabel questionMarkThresholdLabel = new JLabel();
 		questionMarkThresholdLabel.setText("<html><b><font size= '+1'>Weak causality threshold:</font></b><br>lower bound for a weak causality relation between two activities; <br>set to 100% to avoid uncertain edges.</html>");
 		this.add(questionMarkThresholdLabel);
 
-/*		final JLabel questionMarkThresholdLabel2 = new JLabel();
-		questionMarkThresholdLabel2.setText("(lower bound for a weak relation between two activities:");
-		label3.add(questionMarkThresholdLabel2);
-
-		final JLabel questionMarkThresholdLabel3 = new JLabel();
-		questionMarkThresholdLabel3.setText("set to 100% avoid unsure arcs)");
-		label3.add(questionMarkThresholdLabel3);
-		
-		this.add(label3);
-*/
-		//space = Box.createHorizontalStrut(10);
-		//this.add(space);
-		
 		final JSlider questionMarkThresholdSlider = new JSlider();
 		questionMarkThresholdSlider.setMinimum(0);
 		questionMarkThresholdSlider.setMaximum(100);
-		
 		questionMarkThresholdSlider.setMajorTickSpacing(50);
 		questionMarkThresholdSlider.setMinorTickSpacing(10);
 		questionMarkThresholdSlider.setPaintTicks(true);
-		
 		int unsureValue = (int) (settings.getQuestionMarkThreshold()*100);
 		questionMarkThresholdSlider.setValue(unsureValue);
 		Hashtable<Integer, JLabel> unsureLabelTable = new Hashtable<Integer, JLabel>();
-		/*unsureLabelTable.put(0, new JLabel("0%"));
-		unsureLabelTable.put(50, new JLabel("50%"));
-		unsureLabelTable.put(100, new JLabel("100%"));*/
 		unsureLabelTable.put(unsureValue, new JLabel(unsureValue +"%"));
 		questionMarkThresholdSlider.setLabelTable(unsureLabelTable);
 		questionMarkThresholdSlider.setPaintLabels(true);
@@ -223,8 +155,6 @@ public class CGDialog extends JPanel {
 			}
 		});
 		this.add(questionMarkThresholdSlider);
-
-
 		
 		space = Box.createVerticalStrut(20);
 		this.add(space);
@@ -239,11 +169,9 @@ public class CGDialog extends JPanel {
 		final JSlider longDepThresholdSlider = new JSlider();
 		longDepThresholdSlider.setMinimum(0);
 		longDepThresholdSlider.setMaximum(100);
-		
 		longDepThresholdSlider.setMajorTickSpacing(50);
 		longDepThresholdSlider.setMinorTickSpacing(10);
 		longDepThresholdSlider.setPaintTicks(true);
-		
 		int longDepValue = (int) (settings.getLongDepThreshold()*100);
 		longDepThresholdSlider.setValue(longDepValue);
 		Hashtable<Integer, JLabel> longDepLabelTable = new Hashtable<Integer, JLabel>();
@@ -263,35 +191,16 @@ public class CGDialog extends JPanel {
 		});
 		this.add(longDepThresholdSlider);
 
-
-		
 		space = Box.createVerticalStrut(20);
 		this.add(space);
 		
 		space = Box.createHorizontalStrut(10);
 		this.add(space);
 
-
-		//final JPanel label4 = new JPanel(new GridLayout(3,1,1,-10));
-
 		final JLabel causalityWeightLabel = new JLabel();
 		causalityWeightLabel.setText("<html><b><font size= '+1'>Causality weight threshold:</font></b><br>high values mean more emphasis on the split and join behavior of activities; <br>low values mean more emphasis on the detection of concurrency and loops.</html>");
 		this.add(causalityWeightLabel);
 
-/*		final JLabel causalityWeightLabel2 = new JLabel();
-		causalityWeightLabel2.setText("(the lower the value, the more emphasis on concurrency detection: ");
-		label4.add(causalityWeightLabel2);
-
-		final JLabel causalityWeightLabel3 = new JLabel();
-		causalityWeightLabel3.setText("set to 100% to focus on directly follows relation only)");
-		label4.add(causalityWeightLabel3);
-		
-		this.add(label4);*/
-
-		
-		//space = Box.createHorizontalStrut(10);
-		//this.add(space);
-		
 		final JSlider causalityWeightSlider = new JSlider();
 		causalityWeightSlider.setMinimum(0);
 		causalityWeightSlider.setMaximum(100);
@@ -300,9 +209,6 @@ public class CGDialog extends JPanel {
 		causalityWeightSlider.setPaintTicks(true);
 		int causalityValue = (int) (settings.getCausalityWeight()*100);
 		Hashtable<Integer, JLabel> causalitylabelTable = new Hashtable<Integer, JLabel>();
-		/*causalitylabelTable.put(0, new JLabel("0%"));
-		causalitylabelTable.put(50, new JLabel("50%"));
-		causalitylabelTable.put(100, new JLabel("100%"));*/
 		causalitylabelTable.put(causalityValue, new JLabel(causalityValue +"%"));
 		causalityWeightSlider.setLabelTable(causalitylabelTable);
 		causalityWeightSlider.setPaintLabels(true);
@@ -320,29 +226,8 @@ public class CGDialog extends JPanel {
 		});
 		this.add(causalityWeightSlider);
 
-
-		
 		space = Box.createVerticalStrut(20);
 		this.add(space);
-
-
-		/*final JLabel allConnected = new JLabel();
-		allConnected.setText("All tasks connected");
-		this.add(allConnected);
-
-		space = Box.createHorizontalStrut(10);
-		this.add(space);
-
-		JCheckBox connCbx = new JCheckBox();
-		connCbx.setBackground(Color.GRAY);
-		connCbx.setSelected(settings.getHmSettings().isUseAllConnectedHeuristics());
-		connCbx.addChangeListener(new ChangeListener() {
-			public void stateChanged(ChangeEvent e) {
-				HeuristicsMinerSettings hMS = settings.getHmSettings();
-				//hMS.setUseAllConnectedHeuristics(connCbx.isSelected());
-				settings.setHmSettings(hMS);
-			}
-		});
-		this.add(connCbx);*/
 	}
+	
 }
--- src/org/processmining/extendedhybridminer/dialogs/HybridPNDialog.java
+++ src/org/processmining/extendedhybridminer/dialogs/HybridPNDialog.java
@@ -23,118 +23,22 @@ import org.processmining.extendedhybridminer.models.hybridpetrinet.FitnessType;
 import org.processmining.extendedhybridminer.plugins.HybridPNMinerSettings;
 
 
-public class HybridPNDialog extends JPanel 
-{
+public class HybridPNDialog extends JPanel {
 
 	JPanel timeoutPanel,orderingStrategyPanel,fTypePanel,placeNumberPanel, stopPanel;
 	JCheckBox timeoutBox,placeoutBox;
 	JComboBox fType;
 	JTextField timeoutField,placeNumberField,inputPlaceField,outputPlaceField,maxPlaceField, stopField;
 	
-	
 	private static final long serialVersionUID = -3587595452197538653L;
 	@SuppressWarnings("unused")
-	//private FuzzyCGConfiguration configuration;
 	private HybridPNMinerSettings settings;
 
-	public HybridPNDialog(UIPluginContext context, ExtendedCausalGraph fCG, HybridPNMinerSettings settings)
-	{
-		//super(new GridLayout(6,2,0,10));
+	public HybridPNDialog(UIPluginContext context, ExtendedCausalGraph fCG, HybridPNMinerSettings settings) {
+		
 		super(new GridLayout(3, 2,80,30));
 		super.setSize(WIDTH, HEIGHT/2);
 		this.settings = settings;
-		
-		
-		/**
-		 * Additional code by sabi
-		 */
-		/*timeoutPanel=new JPanel();
-		timeoutPanel=new JPanel(new GridLayout(1,1,10,10));
-		timeoutPanel.setBorder(new EmptyBorder(20, 20, 20, 20) );
-		JLabel timeoutLabel=new JLabel();
-		timeoutLabel.setText("<html>Time bound (minutes):</html>");
-		timeoutField=new JTextField(10);
-		int timeout = settings.getMaxTime();
-		timeoutField.setText(String.valueOf(timeout/60));
-		timeoutField.getDocument().addDocumentListener(new DocumentListener()
-				{
-					public void changedUpdate(DocumentEvent e){ check(); }
-					public void removeUpdate(DocumentEvent e) { check(); }
-					public void insertUpdate(DocumentEvent e) { check(); }
-					private void check()
-					{
-						String value=timeoutField.getText().trim();
-						settings.setMaxTime(Integer.parseInt(value.trim())*60);
-					}
-				});
-		timeoutPanel.add(timeoutField);
-		
-		
-		GridBagConstraints c1 = new GridBagConstraints();
-		c1.fill = GridBagConstraints.HORIZONTAL;
-		c1.gridx = 0;
-		c1.gridy = 0;
-		c1.ipady = 20;
-		this.add(timeoutLabel, c1);
-		
-		GridBagConstraints c2 = new GridBagConstraints();
-		c2.fill = GridBagConstraints.HORIZONTAL;
-		c2.gridx = 1;
-		c2.gridy = 0;
-		c2.ipady = 20;
-		c2.gridwidth = 2;
-		this.add(timeoutPanel, c2);
-		
-//		this.add(timeoutLabel);
-//		this.add(timeoutPanel);
-
-		placeNumberPanel=new JPanel(new GridLayout(1,1,10,10));
-		placeNumberPanel.setBorder(new EmptyBorder(20, 20, 20, 20) );
-		JLabel placeNumberLabel=new JLabel();
-		placeNumberLabel.setText("<html>Place size bound:</html>");
-		placeNumberField=new JTextField(10);
-		int placeNumber = settings.getPlaceNumber();
-		placeNumberField.setText(String.valueOf(placeNumber));
-
-		placeNumberField.getDocument().addDocumentListener(new DocumentListener()
-		{
-			public void changedUpdate(DocumentEvent e){ check(); }
-			public void removeUpdate(DocumentEvent e) { check(); }
-			public void insertUpdate(DocumentEvent e) { check(); }
-			private void check()
-			{
-				String value=placeNumberField.getText();
-				if(value.trim().matches("^[0-9]*$"))
-				{
-					settings.setPlaceNumber(Integer.parseInt(value));
-				}
-			}
-		});
-		//placeNumberPanel.add(new JLabel());		
-		//placeNumberPanel.add(new JLabel());
-		//placeNumberPanel.add(new JLabel());
-		placeNumberPanel.add(placeNumberField);
-		//placeNumberPanel.add(new JLabel());
-		//placeNumberPanel.add(new JLabel());
-		//placeNumberPanel.add(new JLabel());
-		
-		c1 = new GridBagConstraints();
-		c1.fill = GridBagConstraints.HORIZONTAL;
-		//c1.weightx = 2;
-		c1.gridx = 0;
-		c1.gridy = 1;
-		c1.ipady = 20;
-		this.add(placeNumberLabel, c1);
-		
-		c2 = new GridBagConstraints();
-		c2.fill = GridBagConstraints.HORIZONTAL;
-		//c2.weightx = 2;
-		c2.gridx = 1;
-		c2.gridy = 1;
-		c2.ipady = 20;
-		c2.gridwidth = 2;
-		this.add(placeNumberPanel, c2);		
-		*/
 		
 		stopPanel=new JPanel(new GridLayout(1,1,10,10));
 		stopPanel.setBorder(new EmptyBorder(20, 20, 20, 20) );
@@ -143,21 +47,16 @@ public class HybridPNDialog extends JPanel
 		stopField=new JTextField(10);
 		int stopNumber = settings.getThresholdEarlyCancelationIterator();
 		stopField.setText(String.valueOf(stopNumber));
-
-		stopField.getDocument().addDocumentListener(new DocumentListener()
-		{
+		stopField.getDocument().addDocumentListener(new DocumentListener() {
 			public void changedUpdate(DocumentEvent e){ check(); }
 			public void removeUpdate(DocumentEvent e) { check(); }
 			public void insertUpdate(DocumentEvent e) { check(); }
-			private void check()
-			{
+			private void check() {
 				String value=stopField.getText();
 				settings.setThresholdEarlyCancelationIterator(Integer.parseInt(value));
 			}
 		});
 		stopPanel.add(stopField);
-
-	
 		this.add(stopLabel);
 		this.add(stopPanel);	
 		
@@ -166,10 +65,6 @@ public class HybridPNDialog extends JPanel
 		JLabel conflictsLabel=new JLabel();		
 		conflictsLabel.setText("<html><b><font size= '+1'>Place evaluation method</font></b></html>");
 		fType = new JComboBox();
-//		conflictsStrategy.addItem("Default conflict strategy");
-//		conflictsStrategy.addItem("Default conflict strategy (optimised)");
-		//conflictsStrategy.setSelectedIndex(1);
-		//conflictsStrategy.addItem("No conflict strategy");
 		int idx = 1;
 		switch(settings.getFitnessType()){
 			case LOCAL:
@@ -179,12 +74,9 @@ public class HybridPNDialog extends JPanel
 				idx =1;
 				break;
 		}
-		
 		fType.addItem("local evaluation");
 		fType.addItem("local evaluation with global fitness guarantee");
-
 		fType.setSelectedIndex(idx);
-		
 		fType.addActionListener(new ActionListener()
 					{
 						@Override
@@ -204,28 +96,17 @@ public class HybridPNDialog extends JPanel
 							}
 						}
 					});
-		
-		
 		fTypePanel.add(conflictsLabel);
 		fTypePanel.add(fType);
-		//this.add(conflictsLabel);
-		//this.add(conflictsPanel);
 		this.add(conflictsLabel);
-		
 		this.add(fTypePanel);
-		
-		//Component space = Box.createHorizontalStrut(10);
 		
 		final JLabel placeEvaluationThresholdLabel = new JLabel();
 		placeEvaluationThresholdLabel.setText("<html><b><font size= '+1'>Fitness threshold for the place evaluation method</font></b></html>");
-		//this.add(placeEvaluationThresholdLabel);
-
-		
 		final JSlider placeEvaluationThresholdSlider = new JSlider();
 		placeEvaluationThresholdSlider.setBorder(new EmptyBorder(20, 20, 20, 20) );
 		placeEvaluationThresholdSlider.setMinimum(0);
 		placeEvaluationThresholdSlider.setMaximum(100);
-		
 		placeEvaluationThresholdSlider.setMajorTickSpacing(50);
 		placeEvaluationThresholdSlider.setMinorTickSpacing(10);
 		placeEvaluationThresholdSlider.setPaintTicks(true);
@@ -236,24 +117,13 @@ public class HybridPNDialog extends JPanel
 		labelTable.put(100, new JLabel("100%"));
 		placeEvaluationThresholdSlider.setLabelTable(labelTable);
 		placeEvaluationThresholdSlider.setPaintLabels(true);
-		
-//		int intValue = (int) (settings.getPlaceEvalThreshold()*100);
-//		placeEvaluationThresholdSlider.setValue(intValue);
 		placeEvaluationThresholdSlider.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				settings.setPlaceEvalThreshold(placeEvaluationThresholdSlider.getValue()/100.0);
 			}
 		});
-		//this.add(placeEvaluationThresholdSlider);		
-		
 		this.add(placeEvaluationThresholdLabel);
-		
 		this.add(placeEvaluationThresholdSlider);
-		
-
-				
-
 	}
-	
 
 }
